8bfe3e4ee013e4d85d05f53f72ff6bd6583ba279
平田祥一朗 <hirasho@hiratashouichirounoMacBook-ea.local>
2018-10-09 17:44:01 +0900

    rails06 task first commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,7 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
+  include CommonActions
 
+  protect_from_forgery with: :exception
   add_flash_types :success, :info, :warning, :danger
+
 end
