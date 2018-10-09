0136830dd2806a1c71ac01cce55da8f2c3bb63d1
平田祥一朗 <hirasho@hiratashouichirounoMacBook-ea.local>
2018-10-09 13:42:50 +0900

    Rails05 first commit

---
+++ app/models/user.rb
@@ -0,0 +1,11 @@
+class User < ApplicationRecord
+  VALED_PASSWORD_REGEX = /([0-9].*[a-zA-Z].*|[a-zA-Z].*[0-9].*)/
+  VALED_EMAIL_REGEX = /.+@.+\..+/
+
+  validates :name, presence: true, length: { maximum: 15}
+  validates :email, presence: true, format: {with: VALED_EMAIL_REGEX}
+  validates :password, length: { in: 8..32}, format: {with: VALED_PASSWORD_REGEX}
+  validates :password_confirmation, length: { in: 8..32}, format: {with: VALED_PASSWORD_REGEX}
+
+  has_secure_password
+end
